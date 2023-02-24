1d059eea42b64316305434875a051eda930e2099
HMT <82308907+HMT2002@users.noreply.github.com>
2023-02-24 21:20:26 +0700

    add params

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const posts = JSON.parse(fs.readFileSync('./json-resources/posts.json'));
-const client_posts = JSON.parse(fs.readFileSync('./json-resources/client_posts.json'));
+// const client_posts = JSON.parse(fs.readFileSync('./json-resources/client_posts.json'));
 
 app.use(express.json());
 app.get('/', (req, res) => {
@@ -19,16 +19,37 @@ app.get('/api/v1/posts', (req, res) => {
   });
 });
 
+app.get('/api/v1/posts/:id/:n?', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id;
+
+  const post = posts.find((el) => el._id.$oid === id);
+
+  if (post === undefined || !post) {
+    return res.status(401).json({
+      status: 'failed',
+      message: 'invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      post: post,
+    },
+  });
+});
+
 app.post('/api/v1/post', (req, res) => {
   // console.log(req.body);
 
-  var numberID = client_posts.length + 1;
+  var numberID = posts.length + 1;
   const newID = 'posts_' + numberID;
   // console.log(newID);
   const newPost = Object.assign({ _id: newID }, req.body);
   console.log(newPost);
-  client_posts.push(newPost);
-  fs.writeFile('./json-resources/client_posts.json', JSON.stringify(client_posts), (err) => {
+  posts.push(newPost);
+  fs.writeFile('./json-resources/posts.json', JSON.stringify(posts), (err) => {
     res.status(201).json({
       status: 'success create',
     });
