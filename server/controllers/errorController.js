86765158944fcf5dc8d390678ddb47a2ac479f0c
HMT <82308907+HMT2002@users.noreply.github.com>
2023-03-28 23:24:18 +0700

    better error handling

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,25 +1,6 @@
 const AppError = require('../utils/appError');
 
-const handleCastErrorDB = (err) => {
-  const message = `Invalid ${err.path}: ${err.value}.`;
-  return new AppError(message, 400);
-};
-
-const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  console.log(value);
-
-  const message = `Duplicate field value: ${value}. Please use another value!`;
-  return new AppError(message, 400);
-};
-const handleValidationErrorDB = (err) => {
-  const errors = Object.values(err.errors).map((el) => el.message);
-
-  const message = `Invalid input data. ${errors.join('. ')}`;
-  return new AppError(message, 400);
-};
-
-const handleJWTError = (err) => new AppError('You are not login', 401);
+const handleJWTError = () => new AppError('You are not login', 401);
 
 const sendError = (err, res) => {
   res.status(err.statusCode).json({
@@ -34,12 +15,11 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  // console.log(err);
   let error = { ...err };
 
   // if (error.name === 'CastError') error = handleCastErrorDB(error);
   // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-  // if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
-
-  sendError(err, res);
+  if (error.name === 'ValidationError') error = handleJWTError();
+  console.log(error);
+  sendError(error, res);
 };
